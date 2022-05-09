0bf7bff26685be983fec003136d4cd8893c9b121
PauloUbirajara <ubirajara3@hotmail.com>
2022-05-09 17:20:28 -0300

    Ajustar nome de variáveis e adicionar mais informações ao log

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 from sys import argv
+
+import pandas as pd
 from gooey import Gooey, GooeyParser
 
-from helper.logging_helper import info, error
+from helper.logging_helper import info, warn, error
 from service.api_service import APIService
 from service.google_scholar_service import GoogleScholarService
 from src.strings.gooey_strings import description, language, program_name
 
 spreadsheet_filters = [
-    "Todos os arquivos|*.*",
+    "Planilhas .xls|*.xls",
     "Planilhas .xlsx|*.xlsx",
-    "Planilhas .csv|*.csv",
-    "Planilhas .xls|*.xls"
+    "Planilhas .csv|*.csv"
 ]
 
 
@@ -23,7 +24,7 @@ def render_gui(api: APIService):
     parser = GooeyParser()
 
     parser.add_argument(
-        'planilha',
+        'spreadsheet',
         metavar='Planilha',
         widget="FileChooser",
         help="Selecione o arquivo contendo os links dos pesquisadores",
@@ -32,23 +33,35 @@ def render_gui(api: APIService):
         }
     )
     parser.add_argument(
-        'coluna',
+        'column',
         metavar='Coluna',
         help="Nome da coluna onde estão os links dos pesquisadores",
         type=str
     )
+
+    # TODO
+    #   Ajustar de DirChooser para SaveFileChooser
+    parser.add_argument(
+        'output_folder',
+        metavar='Pasta de saída',
+        widget="DirChooser",
+        help="Pasta onde a nova planilha contendo as citações será gerada",
+        gooey_options={
+            'default_name': 'out.xls',
+        }
+    )
     args = parser.parse_args(argv[1:])
     info(f'{args}')
-    planilha, coluna = args.planilha, args.coluna
+    spreadsheet, column, output_folder = args.spreadsheet, args.column, args.output_folder
 
     try:
-        if not planilha:
+        if not spreadsheet:
             err_message = "Não foi definido corretamente a planilha com os pesquisadores!"
             raise TypeError(err_message)
 
         user_args_message = f'''
-        Planilha selecionada: {planilha}
-        Coluna selecionada: {coluna}
+        Planilha selecionada: {spreadsheet}
+        Coluna selecionada: {column}
         '''
         info(user_args_message)
         print(user_args_message)
@@ -66,9 +79,8 @@ def render_gui(api: APIService):
             warn(warn_message)
 
         result = f'''
-        Citações obtidas com sucesso!
         Planilha final:
-        {df_planilha.to_string()}
+        {df_spreadsheet.to_string()}
         '''
         info(result)
         print(result)
