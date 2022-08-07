584831bb1565ee71af248ba1ac8e8d18630a6c9f
PauloUbirajara <ubirajara3@hotmail.com>
2022-08-06 21:09:02 -0600

    Alterar atributo para ter citações desde 2015 e id

--- src/model/scholar_info.py
+++ src/model/scholar_info.py
@@ -1,17 +1,27 @@
+from typing import Dict
+
+
 class ScholarInfo:
+    id: str
     h_index: int
     h10_index: int
-    current_year_citations: int
-    previous_5year_citations: int
+    citations_dict: Dict[str, int]
+
+    def __init__(self, researcher_id: str):
+        self.id = researcher_id
+        self.h_index = 0
+        self.h10_index = 0
+        self.citations_dict = {}
+
+    def __str__(self):
+        return '\n'.join([f'id: {self.id}', f'h_index: {self.h_index}', f'i10_index: {self.h10_index}',
+                          f'citation_dict: {self.citations_dict}'])
 
     def set_h_index(self, new_h_index: int):
         self.h_index = new_h_index
 
-    def set_h10_index(self, new_h10_index: int):
+    def set_i10_index(self, new_h10_index: int):
         self.h10_index = new_h10_index
 
-    def set_current_year_citations(self, new_current_year_citations: int):
-        self.current_year_citations = new_current_year_citations
-
-    def set_previous_5year_citations(self, new_previous_5year_citations: int):
-        self.previous_5year_citations = new_previous_5year_citations
+    def set_citations_dict(self, new_citations_dict: Dict[str, int]):
+        self.citations_dict = new_citations_dict
